d7dd06dcddad76d3e9afda1e0ec555ae3051a9c4
samuelssnider <sam_redins@yahoo.com>
2018-08-01 16:28:37 -0600

    lightrail

--- javascript/my_js.js
+++ javascript/my_js.js
@@ -1,2 +1,16 @@
 
-var guess_field = document.getElementsByClassName('guess-field');
+var guessField = document.getElementsByClassName('guess-field');
+var randomNum
+guess_field.addEventListener('keypress', , function (e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) {
+      
+    }
+});
+
+
+function (e) {
+  if (13 == e.keyCode) {
+     ... do whatever ...
+  }
+}
