912526fb300d0b904d68753227bf72f3d1312c3c
samuelssnider <sam_redins@yahoo.com>
2018-08-03 07:54:41 -0600

    more lightrail work

--- javascript/my_js.js
+++ javascript/my_js.js
@@ -1,4 +1,5 @@
-
+var randomNum = (Math.floor(Math.random() * 100)+ 1);
+console.log(randomNum)
 function ready(callback){
     // in case the document is already rendered
     if (document.readyState!='loading') callback();
@@ -31,11 +32,33 @@ var addEnterListener = function (guessField, output, randomNum){
   });
 }
 
+var addGuessButtonListener = function (guessButton, guessField, output, randomNum){
+  guessButton.addEventListener('click', function (e) {
+    var text = ''
+    var userGuess = parseInt(guessField.value)
+    switch (true) {
+      case userGuess > randomNum:
+        text = "Too High";
+        break;
+      case userGuess < randomNum:
+        text = "Too Low";
+        break;
+      case userGuess === randomNum:
+        text = "You Guessed It Buddy!";
+        break;
+      output.innerText = text;
+    }
+  });
+}
+
+
 var addListeners = function(e) {
   var guessField = document.querySelector('.guess-field');
+  var guessButton = document.querySelector('.guess-btn');
+  var clearButton = document.querySelector('.clear-btn');
   var output = document.querySelector('.output')
-  var randomNum = (Math.floor(Math.random() * 100)+ 1);
   addEnterListener (guessField, output, randomNum);
+  addGuessButtonListener(guessButton, guessField, output, randomNum)
 }
 
 ready(function(){
